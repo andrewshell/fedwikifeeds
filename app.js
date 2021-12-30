af18f6cb193ac2d85dcd11656d75d6b6515797df
Andrew Shell <andrew@andrewshell.org>
2021-12-30 16:28:00 -0600

    Refactoring, adding rsscloud and fetching from search.fed.wiki.org

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
 const config = require('./config');
+const createError = require('http-errors');
+const fedwikiHelper = require('./lib/fedwiki-helper');
+const feedHelper = require('./lib/feed-helper');
+const fs = require('fs');
+const everyMinute = require('./lib/every-minute');
+const express = require('express');
+const indexRouter = require('./routes/index');
+const path = require('path');
+const pageCache = require('./lib/page-cache');
+
+if (false === fs.existsSync(config.datadir)) {
+  fs.mkdirSync(config.datadir, { recursive: true });
+}
 
 const app = express();
 
@@ -30,6 +39,34 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+let lastHour = 0, allDomains = [];
+
+everyMinute(async (expectedCycleTime) => {
+  let allFeeds;
+
+  console.log('everyMinute: ' + new Date(expectedCycleTime));
+
+  if (0 === allDomains.length) {
+    allDomains = (await fedwikiHelper.fetchSearchRoster()).data;
+  }
+
+  for (const domain of allDomains.splice(0, 5)) {
+    await feedHelper.fetchPeersOpml(domain);
+  }
+
+  if (expectedCycleTime > lastHour + 3600000) {
+    lastHour = expectedCycleTime;
+    allFeeds = Object.values((await fedwikiHelper.fetchAllFeeds()).data);
+  } else {
+    allFeeds = allFeeds = Object.values((await fedwikiHelper.fetchAllFeeds()).data)
+      .filter(filter => { return filter.active || false; });
+  }
+
+  for (const feed of allFeeds) {
+    await feedHelper.fetchSiteRss(feed.text);
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../config');
-const getIfNew = require('../lib/get-if-new');
-const pageCache = require('../lib/page-cache');
-const fedHelper = require('../lib/fed-helper');
-const rss = require ("daverss");
-const opml = require ("opml");
+const feedHelper = require('../lib/feed-helper');
 
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -14,151 +10,36 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/allfeeds.opml', async function (req, res, next) {
-  const domain = req.params.domain;
-
-  let allfeeds = await fedHelper.fetchAllFeeds();
-
-  const opmlstruct = {
-    opml: {
-      head: {
-        title: `Peers of ${domain}`
-      },
-      body: {
-        subs: allfeeds.data
-      }
-    }
-  };
-
-  cache = await pageCache.write(`/${domain}/peers.opml`, opml.stringify(opmlstruct));
-
+function sendCachedOutput(req, res, cache, contentType) {
   const ifNoneMatch = req.get('if-none-match');
   const ifModifiedSince = req.get('if-modified-since');
 
-  if (ifNoneMatch && cache.etag === ifNoneMatch) {
+  if (false === cache) {
+    res.status(404).send('404 Not Found');
+  } else if (ifNoneMatch && cache.etag === ifNoneMatch) {
     res.status(304).send();
   } else if (ifModifiedSince && cache.created <= (new Date(ifModifiedSince))) {
     res.status(304).send();
   } else {
-    res.header("Content-Type", "text/xml");
+    res.header("Content-Type", contentType);
     res.header("ETag", cache.etag);
     res.status(200).send(cache.data);
   }
+}
+
+router.get('/allfeeds.opml', async function (req, res, next) {
+  let cache = await feedHelper.fetchAllFeeds();
+  sendCachedOutput(req, res, cache, 'text/xml');
 });
 
 router.get('/:domain/peers.opml', async function (req, res, next) {
-  const domain = req.params.domain;
-  const peers = await fedHelper.fetchPeers(domain);
-
-  let cache = await pageCache.read(`/${domain}/peers.opml`);
-  let allfeeds = await fedHelper.fetchAllFeeds();
-
-  if (false !== peers && (false === peers.cached || false === cache)) {
-    const opmlstruct = {
-      opml: {
-        head: {
-          title: `Peers of ${domain}`
-        },
-        body: {
-          subs: await Promise.all(peers.data.map(async (peer) => {
-            const homepage = await fedHelper.homepageUrl(peer);
-            const feed = {
-              type: 'rss',
-              text: peer,
-              xmlUrl: `${config.docroot}/${peer}/rss.xml`,
-              description: `Updates from ${peer} fedwiki`,
-              htmlUrl: `${homepage}/`,
-              version: 'RSS2'
-            };
-            allfeeds.data.push(feed);
-            return feed;
-          }))
-        }
-      }
-    };
-
-    cache = await pageCache.write(`/${domain}/peers.opml`, opml.stringify(opmlstruct));
-    await fedHelper.saveAllFeeds(allfeeds.data);
-  }
-
-  const ifNoneMatch = req.get('if-none-match');
-  const ifModifiedSince = req.get('if-modified-since');
-
-  if (ifNoneMatch && cache.etag === ifNoneMatch) {
-    res.status(304).send();
-  } else if (ifModifiedSince && cache.created <= (new Date(ifModifiedSince))) {
-    res.status(304).send();
-  } else {
-    res.header("Content-Type", "text/xml");
-    res.header("ETag", cache.etag);
-    res.status(200).send(cache.data);
-  }
+  let cache = await feedHelper.fetchPeersOpml(req.params.domain);
+  sendCachedOutput(req, res, cache, 'text/xml');
 });
 
 router.get('/:domain/rss.xml', async function(req, res, next) {
-  const domain = req.params.domain;
-  const sitemap = await fedHelper.fetchSitemap(domain);
-  const homepage = await fedHelper.homepageUrl(domain);
-  const filterDate = new Date(Date.now() - (7 * 24 * 60 * 60 * 1000)); // One Week
-
-  let cache = await pageCache.read(`/${domain}/rss.xml`);
-  let allfeeds = await fedHelper.fetchAllFeeds();
-
-  if (false !== sitemap && (false === sitemap.cached || false === cache)) {
-    const headElements = {
-      title: domain,
-      link: `${homepage}/`,
-      description: `Updates from ${domain} fedwiki`,
-      generator: config.generator,
-      maxFeedItems: 100,
-      image: {
-        url: `${homepage}/favicon.png`,
-        title: domain,
-        link: `${homepage}/`,
-        width: 32,
-        height: 32
-      }
-    };
-
-    const historyArray = sitemap.data.map((page) => {
-      return {
-        title: page.title,
-        text: page.synopsis,
-        link: `${homepage}/${page.slug}.html`,
-        when: (new Date(page.date)),
-        guid: {
-          flPermalink: false,
-          value: `${homepage}/${page.slug}.html#${page.date}`
-        }
-      };
-    }).filter((item) => {
-      return item.when > filterDate;
-    }).sort((a, b) => b.when - a.when);
-
-    cache = await pageCache.write(`/${domain}/rss.xml`, rss.buildRssFeed(headElements, historyArray));
-    allfeeds.data.push({
-      type: 'rss',
-      text: domain,
-      xmlUrl: `${config.docroot}/${domain}/rss.xml`,
-      description: `Updates from ${domain} fedwiki`,
-      htmlUrl: `${homepage}/`,
-      version: 'RSS2'
-    });
-    await fedHelper.saveAllFeeds(allfeeds.data);
-  }
-
-  const ifNoneMatch = req.get('if-none-match');
-  const ifModifiedSince = req.get('if-modified-since');
-
-  if (ifNoneMatch && cache.etag === ifNoneMatch) {
-    res.status(304).send();
-  } else if (ifModifiedSince && cache.created <= (new Date(ifModifiedSince))) {
-    res.status(304).send();
-  } else {
-    res.header("Content-Type", "application/rss+xml");
-    res.header("ETag", cache.etag);
-    res.status(200).send(cache.data);
-  }
+  let cache = await feedHelper.fetchSiteRss(req.params.domain);
+  sendCachedOutput(req, res, cache, 'application/rss+xml');
 });
 
 module.exports = router;
