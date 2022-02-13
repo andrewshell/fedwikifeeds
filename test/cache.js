192c135d683c66aed130068687914334dbc4d295
Andrew Shell <andrew@andrewshell.org>
2022-02-13 12:52:57 -0600

    Switching to cacheDomain, cachePath at top level

--- test/cache.js
+++ test/cache.js
@@ -18,13 +18,13 @@ describe('cache', function() {
   describe('when status=onlyFresh', async function () {
 
     it('should return a Hit with the live value', async function () {
-      const c = await cache('_internal/nocache', cache.status.onlyFresh, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.onlyFresh, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('cached', false);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -34,26 +34,26 @@ describe('cache', function() {
     });
 
     it('should return a Miss on error', async function () {
-      const c = await cache('_internal/nocache', cache.status.onlyFresh, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.onlyFresh, async () => {
         throw Error('cache error');
       });
 
       expect(c).to.be.a(cacheStoreCommon.Miss);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('error');
       expect(c.error).to.be.an(Error);
       expect(c.error).to.have.property('message', 'cache error');
     });
 
     it('should update cache with successful live value', async function () {
-      const c = await cache('_internal/true', cache.status.onlyFresh, async () => {
+      const c = await cache('_internal', 'true', cache.status.onlyFresh, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', false);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -61,10 +61,12 @@ describe('cache', function() {
       expect(c).to.have.property('error', null);
       expect(c).to.have.property('etag');
 
-      expect(cache.store.data).to.have.property('_internal/true');
-      expect(cache.store.data['_internal/true']).to.be.a(cacheStoreCommon.Data);
-      expect(cache.store.data['_internal/true']).to.have.property('data', 'live');
-      expect(cache.store.data['_internal/true']).to.have.property('etag', c.etag);
+      expect(cache.store.data).to.have.property('-internal/true');
+
+      const d = cacheStoreCommon.Data.parse(cache.store.data['-internal/true']);
+      expect(d).to.be.a(cacheStoreCommon.Data);
+      expect(d).to.have.property('data', 'live');
+      expect(d).to.have.property('etag', c.etag);
     });
 
   });
@@ -72,30 +74,30 @@ describe('cache', function() {
   describe('when status=cacheOnFail', function () {
 
     it('should return a Miss on error with no cache', async function () {
-      const c = await cache('_internal/nocache', cache.status.cacheOnFail, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.cacheOnFail, async () => {
         throw Error('cache error');
       });
 
       expect(c).to.be.a(cacheStoreCommon.Miss);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('error');
       expect(c.error).to.be.an(Error);
       expect(c.error).to.have.property('message', 'cache error');
     });
 
     it('should return a Hit on error with cache', async function () {
-      cache.store.data['_internal/true'] = cacheStoreCommon.Data.fromHit(
-        new cacheStoreCommon.Hit('_internal/true', 'cached')
+      cache.store.data['-internal/true'] = cacheStoreCommon.Data.fromHit(
+        new cacheStoreCommon.Hit('-internal/true', 'cached')
       ).stringify();
 
-      const c = await cache('_internal/true', cache.status.cacheOnFail, async () => {
+      const c = await cache('_internal', 'true', cache.status.cacheOnFail, async () => {
         throw Error('cache error');
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', true);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -107,17 +109,17 @@ describe('cache', function() {
     });
 
     it('should return a Hit with the live value with cache', async function () {
-      cache.store.data['_internal/true'] = cacheStoreCommon.Data.fromHit(
-        new cacheStoreCommon.Hit('_internal/true', 'cached')
+      cache.store.data['-internal/true'] = cacheStoreCommon.Data.fromHit(
+        new cacheStoreCommon.Hit('-internal/true', 'cached')
       ).stringify();
 
-      const c = await cache('_internal/true', cache.status.cacheOnFail, async () => {
+      const c = await cache('_internal', 'true', cache.status.cacheOnFail, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', false);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -127,17 +129,17 @@ describe('cache', function() {
     });
 
     it('should update cache with successful live value', async function () {
-      cache.store.data['_internal/true'] = cacheStoreCommon.Data.fromHit(
-        new cacheStoreCommon.Hit('_internal/true', 'cached')
+      cache.store.data['-internal/true'] = cacheStoreCommon.Data.fromHit(
+        new cacheStoreCommon.Hit('-internal/true', 'cached')
       ).stringify();
 
-      const c = await cache('_internal/true', cache.status.cacheOnFail, async () => {
+      const c = await cache('_internal', 'true', cache.status.cacheOnFail, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', false);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -145,10 +147,12 @@ describe('cache', function() {
       expect(c).to.have.property('error', null);
       expect(c).to.have.property('etag');
 
-      expect(cache.store.data).to.have.property('_internal/true');
-      expect(cache.store.data['_internal/true']).to.be.a(cacheStoreCommon.Data);
-      expect(cache.store.data['_internal/true']).to.have.property('data', 'live');
-      expect(cache.store.data['_internal/true']).to.have.property('etag', c.etag);
+      expect(cache.store.data).to.have.property('-internal/true');
+
+      const d = cacheStoreCommon.Data.parse(cache.store.data['-internal/true']);
+      expect(d).to.be.a(cacheStoreCommon.Data);
+      expect(d).to.have.property('data', 'live');
+      expect(d).to.have.property('etag', c.etag);
     });
 
   });
@@ -156,30 +160,30 @@ describe('cache', function() {
   describe('when status=preferCache', function () {
 
     it('should return a Miss on error with no cache', async function () {
-      const c = await cache('_internal/nocache', cache.status.preferCache, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.preferCache, async () => {
         throw Error('cache error');
       });
 
       expect(c).to.be.a(cacheStoreCommon.Miss);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('error');
       expect(c.error).to.be.an(Error);
       expect(c.error).to.have.property('message', 'cache error');
     });
 
     it('should return a Hit with the cached value with cache', async function () {
-      cache.store.data['_internal/true'] = cacheStoreCommon.Data.fromHit(
-        new cacheStoreCommon.Hit('_internal/true', 'cached')
+      cache.store.data['-internal/true'] = cacheStoreCommon.Data.fromHit(
+        new cacheStoreCommon.Hit('-internal/true', 'cached')
       ).stringify();
 
-      const c = await cache('_internal/true', cache.status.preferCache, async () => {
+      const c = await cache('_internal', 'true', cache.status.preferCache, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', true);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -189,13 +193,13 @@ describe('cache', function() {
     });
 
     it('should return a Hit with the live value with no cache', async function () {
-      const c = await cache('_internal/true', cache.status.preferCache, async () => {
+      const c = await cache('_internal', 'true', cache.status.preferCache, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', false);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -208,30 +212,30 @@ describe('cache', function() {
 
   describe('when status=onlyCache', function () {
     it('should return a Miss on error with no cache', async function () {
-      const c = await cache('_internal/nocache', cache.status.onlyCache, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.onlyCache, async () => {
         throw Error('cache error');
       });
 
       expect(c).to.be.a(cacheStoreCommon.Miss);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('error');
       expect(c.error).to.be.an(Error);
       expect(c.error).to.have.property('message', 'Missing cache');
     });
 
     it('should return a Hit with the cached value with cache', async function () {
-      cache.store.data['_internal/true'] = cacheStoreCommon.Data.fromHit(
-        new cacheStoreCommon.Hit('_internal/true', 'cached')
+      cache.store.data['-internal/true'] = cacheStoreCommon.Data.fromHit(
+        new cacheStoreCommon.Hit('-internal/true', 'cached')
       ).stringify();
 
-      const c = await cache('_internal/true', cache.status.onlyCache, async () => {
+      const c = await cache('_internal', 'true', cache.status.onlyCache, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Hit);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/true');
+      expect(c).to.have.property('cacheName', '-internal/true');
       expect(c).to.have.property('cached', true);
       expect(c).to.have.property('created');
       expect(c.created).to.be.a(Date);
@@ -241,13 +245,13 @@ describe('cache', function() {
     });
 
     it('should return a Miss with no cache', async function () {
-      const c = await cache('_internal/nocache', cache.status.onlyCache, async () => {
+      const c = await cache('_internal', 'nocache', cache.status.onlyCache, async () => {
         return 'live';
       });
 
       expect(c).to.be.a(cacheStoreCommon.Miss);
       expect(c).to.have.property('version', 2);
-      expect(c).to.have.property('cacheName', '_internal/nocache');
+      expect(c).to.have.property('cacheName', '-internal/nocache');
       expect(c).to.have.property('error');
       expect(c.error).to.be.an(Error);
       expect(c.error).to.have.property('message', 'Missing cache');
